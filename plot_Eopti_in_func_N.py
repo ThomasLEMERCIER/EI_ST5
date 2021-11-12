657c91c6ac2e4d4c765306afe33dbe747e1ee338
Timothé Boulet <timothe.boulet0@gmail.com>
2021-11-12 15:57:29 +0100

    plot E_opti in func of N

--- plot_Eopti_in_func_N.py
+++ plot_Eopti_in_func_N.py
@@ -20,10 +20,7 @@ def main():
     # ----------------------------------------------------------------------
     # -- Define the structure of the problem
     # ----------------------------------------------------------------------
-    N = 9  # number of points along x-axis
-    M = 2 * N  # number of points along y-axis
     level = 1 # level of the fractal
-    spacestep = 1.0 / N  # mesh size
     c0 = 340
     # -- set parameters of the partial differential equation
     kx = -1.0
@@ -33,42 +30,50 @@ def main():
     omega = wavenumber * c0 
     alpha_precision = 15
 
-    # -- set geometry of domain
-    domain_omega, x, y, _, _ = EI.pde.preprocessing._set_geometry_of_domain(M, N, level)
-
-    # -- define material density matrix
-    chi0 = EI.pde.preprocessing._set_chi(M, N, x, y)
-    chi0 = EI.pde.preprocessing.set2zero(chi0, domain_omega)
     # -- define absorbing material
     Alpha = EI.alpha.compute_alpha(material, omega, alpha_precision)
     Alpha = Alpha[0] + Alpha[1] * 1j
-    alpha_rob = Alpha * chi0
 
 
-    K = 3 
+    K = 3
     # algo_opti.directGradientDescent.DirectGradientDescent_Adam
     algos = [   algo_opti.GradientDescent.evolutive_lr_ProjectedGradientDescent,
                 algo_opti.GradientDescent.ProjectedGradientDescent,
-                algo_opti.GradientDescent.evolutive_lr_ProjectedGradientDescent_Adam,
-                algo_opti.directGradientDescent.DirectGradientDescent,
-                algo_opti.directGradientDescent.DirectGradientDescent_Adam,
                 algo_opti.softGD.soft_evolutive_lr_ProjectedGradientDescent,
                 algo_opti.softGD.soft_evolutive_lr_ProjectedGradientDescent_Adam,
                 algo_opti.softGD.soft_ProjectedGradientDescent,
+                algo_opti.GradientDescent.evolutive_lr_ProjectedGradientDescent_Adam,
+                algo_opti.directGradientDescent.DirectGradientDescent,
+                algo_opti.directGradientDescent.DirectGradientDescent_Adam,
+                
     ]
-    iterations = np.arange(K+1)
+
+    L_N = np.linspace(10, 100, 5).tolist()
     plt.figure()
     for algo in algos:
+        print("Optimizing energy for various N for algo =", algo.__name__)
+        energy_opti = list()
+        for N in L_N:
+            # -- set geometry of domain
+            N = int(N)
+            print("N = ", N)
+            M = 2*N
+            spacestep = 1.0 / N  # mesh size
 
+            domain_omega, x, y, _, _ = EI.pde.preprocessing._set_geometry_of_domain(M, N, level)
 
-        # -- compute optimization
-        chi, energy, u = algo(chi0, domain_omega, spacestep, wavenumber, Alpha, K)
-        # --- en of optimization
+            # -- define material density matrix
+            chi0 = EI.pde.preprocessing._set_chi(M, N, x, y)
+            chi0 = EI.pde.preprocessing.set2zero(chi0, domain_omega)
 
-        plt.plot(iterations, energy, label=algo.__name__)
+            # -- compute optimization
+            chi, energy, u = algo(chi0, domain_omega, spacestep, wavenumber, Alpha, K)
+            # --- en of optimization
 
-    plt.legend()
-    plt.show()
+            energy_opti.append(energy[-1])
+        plt.plot(L_N, energy_opti, label=algo.__name__)
+        plt.legend()
+        plt.savefig("Eopti_in_func_N")
 
 if __name__ == "__main__":
     main()
